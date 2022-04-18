6f609533d929e54ffcabfd9622d4fb414cad0057
Shabasdesigntech <mohammed.shabas@designtech.se>
2022-04-18 15:50:59 +0530

    18-04-2022

---
+++ trianing/todo/destructing.js
@@ -0,0 +1,14 @@
+const employee = {
+    id : 1,
+    name : "dev nm1",
+    age : 30,
+    department:"UI",
+    address:{
+        city:"mst",
+        country:"pk",
+    },
+
+};
+
+const name = employee.name;
+console.log(name);
