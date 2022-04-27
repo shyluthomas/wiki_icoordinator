10fceef833f2a92964f41a09cb39c396addeb2c7
Shabasdesigntech <mohammed.shabas@designtech.se>
2022-04-27 16:11:19 +0530

    27-04-2022

---
+++ trianing/netflixtest/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+function App() {
+  return (
+  <div className="App">
+      <NavBar/>
+      <Banner/>
+      
+
+  </div>
+
+  );
+}
+
+export default App;
---
+++ trianing/netflixtest/src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie</h1>
+                <div className='banner_buttonds'>
+                    <button className='button'>play</button>
+                    <button className='button'>my list</button>
+
+                </div>
+                <h1 className='description'>fdgdfgfdggfgffdgdfgdfgdfgfgfdgfgfg</h1>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Banner
